61ba6e89329a8608830dccfbabb97302c089735e
lucas.ribeiro <lucas.ribeiro@LAB5-DCX-14.dcx.ufpb.br>
2018-04-05 11:16:30 -0300

    Teste com professora

--- src/teste1.java
+++ src/teste1.java
@@ -1,7 +1,8 @@
 
 public class teste1 {
 	public static void main(String[] args){
-		System.out.println("ola mundo");                                                                                                        
+		System.out.println("ola mundo"); 
+		System.out.println("Teste Com a professora"); 
 	}
 
 }
